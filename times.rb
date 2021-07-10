746d6a3dcbc447c782cfba9424640cf73482b599
Megan McCarty <megan_mccarty@hotmail.com>
2021-07-10 17:25:51 -0400

    commit tree 29f872d56938fb04e2aab83423419a03996dea12

--- times.rb
+++ times.rb
@@ -1,7 +1,22 @@
 def using_times
-	#your code here
+	7.times do
+        puts "Wingardium Leviosa"
+    end
 end
 
+7.times do
+    puts "I am doing the dishes left by my former friends."
+end
+
+jewels_in_bag = 100
+
+3.times do 
+    puts "Hiding 10 stolen jewels."
+    jewels_in_bag = jewels_in_bag - 10
+    puts "Now there are only #{jewels_in_bag} jewels left to hide!"
+end
+
+puts "We have #{jewels_in_bag} jewels still to hide!"
 
 
 
